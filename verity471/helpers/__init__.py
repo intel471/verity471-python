9f7b108a260606de5a3a19cfa4761ad8dcbbe8b8
mmolenda <spamm@molenda.eu>
2026-04-10 16:16:18 +0200

    added get_latest helper

--- verity471/helpers/__init__.py
+++ verity471/helpers/__init__.py
@@ -1,4 +1,5 @@
 from verity471.helpers.alerts import AlertTarget, fetch_alert_targets
+from verity471.helpers.stream_latest import get_latest
 from verity471.helpers.url_router import call_url, resolve_url
 
-__all__ = ["AlertTarget", "fetch_alert_targets", "resolve_url", "call_url"]
+__all__ = ["AlertTarget", "fetch_alert_targets", "get_latest", "resolve_url", "call_url"]
